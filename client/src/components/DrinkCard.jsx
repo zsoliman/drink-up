dc48a2c05e038b713a229494fb8b87a4f35328a0
Zachary Soliman <zachsoliman1@gmail.com>
2022-10-05 18:27:28 -0400

    new logo

--- client/src/components/DrinkCard.jsx
+++ client/src/components/DrinkCard.jsx
@@ -110,29 +110,30 @@ const DrinkCard = ({ drink, favoritesList, addToFavorites, deleteFavorite }) => 
                 </div>
 
                 <div className="card__side card__side--back card__side--back-1">
-                    <h3>What you'll need:</h3>
-                    <p>{drink.strMeasure1} {drink.strIngredient1}</p>
-                    <p>{drink.strMeasure2} {drink.strIngredient2}</p>
-                    <p>{drink.strMeasure3} {drink.strIngredient3}</p>
-                    <p>{drink.strMeasure4} {drink.strIngredient4}</p>
-                    <p>{drink.strMeasure5} {drink.strIngredient5}</p>
-                    <p>{drink.strMeasure6} {drink.strIngredient6}</p>
-                    <p>{drink.strMeasure7} {drink.strIngredient7}</p>
-                    <p>{drink.strMeasure8} {drink.strIngredient8}</p>
-                    <p>{drink.strMeasure9} {drink.strIngredient9}</p>
-                    <p>{drink.strMeasure10} {drink.strIngredient10}</p>
-                    <p>{drink.strMeasure11} {drink.strIngredient11}</p>
-                    <p>{drink.strMeasure12} {drink.strIngredient12}</p>
-                    <p>{drink.strMeasure13} {drink.strIngredient13}</p>
-                    <p>{drink.strMeasure14} {drink.strIngredient14}</p>
-                    <p>{drink.strMeasure15} {drink.strIngredient15}</p>
-                    <h3>Instructions:</h3>
-                    <p>{drink.strInstructions}</p>
-                    {/* <p>Tags: {drink.strTags}</p> */}
-
+                    <div className="card-info">
+                        <h3>What you'll need:</h3>
+                        <p>{drink.strMeasure1} {drink.strIngredient1}</p>
+                        <p>{drink.strMeasure2} {drink.strIngredient2}</p>
+                        <p>{drink.strMeasure3} {drink.strIngredient3}</p>
+                        <p>{drink.strMeasure4} {drink.strIngredient4}</p>
+                        <p>{drink.strMeasure5} {drink.strIngredient5}</p>
+                        <p>{drink.strMeasure6} {drink.strIngredient6}</p>
+                        <p>{drink.strMeasure7} {drink.strIngredient7}</p>
+                        <p>{drink.strMeasure8} {drink.strIngredient8}</p>
+                        <p>{drink.strMeasure9} {drink.strIngredient9}</p>
+                        <p>{drink.strMeasure10} {drink.strIngredient10}</p>
+                        <p>{drink.strMeasure11} {drink.strIngredient11}</p>
+                        <p>{drink.strMeasure12} {drink.strIngredient12}</p>
+                        <p>{drink.strMeasure13} {drink.strIngredient13}</p>
+                        <p>{drink.strMeasure14} {drink.strIngredient14}</p>
+                        <p>{drink.strMeasure15} {drink.strIngredient15}</p>
+                        <h3>Instructions:</h3>
+                        <p>{drink.strInstructions}</p>
+                        {/* <p>Tags: {drink.strTags}</p> */}
+                    </div>
                     {user ?
 
-                        <button className={isFavorite ? 'fav' : ''} onClick={handleClick}>Like</button>
+                        <button className={isFavorite ? 'fav' : 'unfav'} onClick={handleClick}>Like</button>
                         :
                         ''
                     }
